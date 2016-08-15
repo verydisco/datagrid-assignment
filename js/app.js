f4e5c6cb4877c04ac8dd06e604352c84c39a0e33
chrismcardle <chrismcardle@eyekiller.com>
2016-08-15 23:34:40 +0100

    modal popup added

--- js/app.js
+++ js/app.js
@@ -15,5 +15,38 @@
 			$scope.propertyName = propertyName;
 		};
 
+		$scope.showAddForm = function () {
+            $scope.modalInstance = $modal.open({
+                templateUrl: '/modal-form.html',
+                controller: ModalInstanceCtrl,
+                scope: $scope,
+                resolve: {
+                    userForm: function () {
+                        return $scope.userForm;
+                    }
+                }
+            });
+        };
+
+        $scope.showEditForm = function (detail) {
+        	console.log(detail);
+            $scope.modalInstance = $modal.open({
+                templateUrl: '/modal-editform.html',
+                controller: ModalInstanceCtrl,
+                scope: $scope,
+                resolve: {
+                    userForm: function () {
+                        return $scope.userForm;
+                    }
+                }
+            });
+        };
 	} ]);
+
+	var ModalInstanceCtrl = function ($scope, $modalInstance) {
+	    $scope.cancel = function () {
+	        $modalInstance.dismiss('cancel');
+	    };
+	};
+	
 })();
